d16721c505be9412b528eefbe72600608ab86c45
Morten Barklund <morten.barklund@gmail.com>
2022-02-05 18:45:36 -0500

    First commit

---
+++ app/routes/index.jsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
+      <h1>Are you ready to take your React knowledge to the next level?</h1>
+    </div>
+  );
+}
